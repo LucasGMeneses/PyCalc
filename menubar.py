330380da716ca26d1bcd5492b38d207e5df43062
LucasGMeneses <48634790+LucasGMeneses@users.noreply.github.com>
2020-06-13 15:28:13 -0400

    Update menubar.py

--- menubar.py
+++ menubar.py
@@ -5,12 +5,12 @@ class Menubar(tk.Menu):
         super().__init__()
         self['bg'] = 'gray30'
         self['fg'] = '#ffffff'
-        
+        self['font'] = 'Arial 10 bold'
         self.__create_optmenu(mst)
         mst.config(menu=self)
 
     def __create_optmenu(self, mst):
-        optmenu = tk.Menu(self, tearoff=0, bg='gray30', fg='#ffffff')
+        optmenu = tk.Menu(self, tearoff=0, bg='gray30', fg='#ffffff', font='Arial 10 bold')
         optmenu.add_command(label='Help')
         optmenu.add_separator()
         optmenu.add_command(label='Exit',command=mst.quit)
